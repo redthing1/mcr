ea2bc99a5e7b697e04918f432ec9a693920ab906
redthing1 <redthing1@alt.icu>
2022-04-15 02:54:30 -0700

    Revert "fix static executable compile"
    This reverts commit 6f0529a94b11a26c40fc22e56aed8263dba173fa.

--- make.sh
+++ make.sh
@@ -1,1 +1,2 @@
-g++ -static -DNDEBUG -O3 -fomit-frame-pointer -msse2 -std=c++0x -D_FILE_OFFSET_BITS=64 -o mcm ./src/CM.cpp ./src/Archive.cpp ./src/Huffman.cpp ./src/MCM.cpp ./src/Memory.cpp ./src/Util.cpp ./src/Compressor.cpp ./src/LZ.cpp -Wl,--whole-archive -lpthread -Wl,--no-whole-archive
+g++ -DNDEBUG -O3 -fomit-frame-pointer -msse2 -std=c++0x -D_FILE_OFFSET_BITS=64 -o mcm \
+    ./src/CM.cpp ./src/Archive.cpp ./src/Huffman.cpp ./src/MCM.cpp ./src/Memory.cpp ./src/Util.cpp ./src/Compressor.cpp ./src/LZ.cpp -lpthread
