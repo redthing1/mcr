21bda8c6bf07d257e613e7b14b1195311b34493b
redthing1 <redthing1@alt.icu>
2022-05-02 19:17:27 -0700

    add march native version

---
+++ make_native.sh
@@ -0,0 +1,2 @@
+g++ -DNDEBUG -O3 -fomit-frame-pointer -march=native -std=c++0x -D_FILE_OFFSET_BITS=64 -o mcr \
+    ./src/CM.cpp ./src/Archive.cpp ./src/Huffman.cpp ./src/Main.cpp ./src/Memory.cpp ./src/Util.cpp ./src/Compressor.cpp ./src/LZ.cpp -lpthread
