42babbe4c1c768ebbcd337d2cc0d8a17172fac59
redthing1 <redthing1@alt.icu>
2023-08-21 19:21:58 -0700

    rename output binary to mcr mac

--- make_native_macos.sh
+++ make_native_macos.sh
@@ -3,7 +3,7 @@
 # overridable compiler
 CXX=${CXX:-g++}
 
-$CXX -DNDEBUG -O3 -march=native -std=c++0x -D_FILE_OFFSET_BITS=64 -o mcr \
+$CXX -DNDEBUG -O3 -march=native -std=c++0x -D_FILE_OFFSET_BITS=64 -o mcr_mac \
     -fomit-frame-pointer -flax-vector-conversions \
     -lstdc++ -pthread \
     ./src/CM.cpp ./src/Archive.cpp ./src/Huffman.cpp ./src/Main.cpp ./src/Memory.cpp ./src/Util.cpp ./src/Compressor.cpp ./src/LZ.cpp
